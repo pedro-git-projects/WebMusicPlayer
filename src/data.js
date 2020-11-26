a97c09b98d79937ab8ad5257b04d58509c2ffc96
unknown <pedro.coding.contact@gmail.com>
2020-11-26 18:11:25 -0300

    Gradient Working

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ function Music() {
         cover: "https://chillhop.com/wp-content/uploads/2020/10/23fdd99adc3e16abcb67b004ea3e748ebf433a49-1024x1024.jpg",
         id: uuidv4(),
         active: true,
-        color: ['#4479BB,#C571BC'],
+        color: ["#4479BB","#C571BC"],
         audio: "https://mp3.chillhop.com/serve.php/?mp3=10448",
     },
     {
@@ -17,7 +17,7 @@ function Music() {
         cover: "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
         id: uuidv4(),
         active: false,
-        color: ['#C6598F,#68A5CA'],
+    color: ["#C6598F","#68A5CA"],
         audio: "https://mp3.chillhop.com/serve.php/?mp3=7961",
     },
         
